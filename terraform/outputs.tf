5631eb4db1edf429e9b3f72ade490bdcc5e24305
Arthur-Busanello09 <149339783+Arthur-Busanello09@users.noreply.github.com>
2025-06-08 18:22:30 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "vm_ip" {
   description = "IP externo da VM criada"
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.semanal_homol.network_interface[0].access_config[0].nat_ip
 }

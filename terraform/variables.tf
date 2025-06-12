ac0c071cf4a3b6b38debc210771d65402354fb9f
JosueObadovski <101779621+JosueObadovski@users.noreply.github.com>
2025-06-12 19:51:05 -0300

    Update variables.tf
    Atualizando update variables.tf para o mensal

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,24 @@
-variable "project_id" {
+variable "env" {
+  description = "Ambiente: prod ou stage"
   type        = string
+}
+
+variable "project_id" {
+  type = string
 }
 
 variable "region" {
-  default     = "us-central1"
+  default = "us-central1"
 }
 
 variable "zone" {
-  default     = "us-central1-a"
+  default = "us-central1-a"
 }
 
 variable "machine_type" {
-  default     = "e2-medium"
-}
-
-variable "image" {
-  default     = "ubuntu-os-cloud/ubuntu-2204-lts"
-}
-
-variable "disk_size" {
-  default     = 20
-}
-
-variable "public_key_path" {
-  default     = "~/.ssh/id_rsa.pub"  
+  default = "e2-medium"
 }
 
 variable "credentials_file_path" {
-  type        = string
+  type = string
 }

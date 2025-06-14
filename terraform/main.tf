0a004d869c956029ffb2f0dd5a076efffb0bfe9b
Arthur Henrique Vieira Busanello <tuti.busanello@hotmail.com>
2025-06-14 19:35:52 -0300

    teste1 kluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,29 +3,64 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-  
 }
 
-resource "google_compute_instance" "semanal_homol" {
-  name         = "vm01_terraform"
-  machine_type = var.machine_type
-  zone         = var.zone
+# Criando o Cluster GKE
+resource "google_container_cluster" "semanal_homol" {
+  name     = "gke-cluster-semanal-homol"
+  location = var.region
+
+  # Número inicial de nós do cluster
+  initial_node_count = 3
   
-  boot_disk {
-    initialize_params {
-      image = var.image         
-      size  = var.disk_size 
+  # Configuração dos nós do cluster
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+
+    metadata = {
+      ssh-keys = "ubuntu:${file(var.public_key_path)}"
     }
+
+    # Definir a imagem base para as instâncias do GKE
+    image_type = "COS"  # Container-Optimized OS
   }
 
- network_interface {
-    network = "default"
-    access_config {}
+  # Habilitando o autoscaling
+  enable_autoscaling = true
+  min_node_count     = 1
+  max_node_count     = 5
+
+  # Rede do cluster
+  network    = "default"
+  subnetwork = "default"
+
+  # Permissões para os nós
+  master_auth {
+    username = "admin"
+    password = "password"
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"  
+  # Habilita o balanceamento de carga do Kubernetes
+  enable_network_policy = true
+}
+
+# Configuração do firewall para permitir o acesso aos serviços do cluster
+resource "google_compute_firewall" "allow_ports" {
+  name    = "allow-observabilidade-v3"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "3000", "9090"]  # Abertura de portas necessárias para o acesso
   }
 
-  tags = ["ssh-conexao", "server"]
+  source_ranges = ["0.0.0.0/0"]  # Isso libera acesso público (use com cautela em produção)
+
+  target_tags = ["ssh-conexao", "server"]
+}
+
+# Output para o kubeconfig
+output "kubeconfig" {
+  value = google_container_cluster.semanal_homol.kube_config
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,40 @@
+# ID do projeto GCP
 variable "project_id" {
   type = string
 }
 
+# Região do GCP (ex: us-central1, us-east1)
 variable "region" {
   default = "us-central1"
 }
 
+# Zona do GCP (ex: us-central1-a)
 variable "zone" {
   default = "us-central1-a"
 }
 
+# Tipo de máquina para os nós do GKE (ex: e2-medium, n1-standard-1)
 variable "machine_type" {
   default = "e2-medium"
 }
 
+# Imagem do sistema operacional para as instâncias dos nós (ex: ubuntu-os-cloud/ubuntu-2204-lts)
 variable "image" {
   default = "ubuntu-os-cloud/ubuntu-2204-lts"
 }
 
+# Tamanho do disco para as instâncias do GKE (em GB)
 variable "disk_size" {
   default = 20
 }
 
+# Caminho para a chave pública SSH usada para acessar os nós
 variable "public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+  type    = string
+  default = "~/.ssh/id_rsa.pub"  # Defina o caminho completo, se necessário
 }
 
+# Caminho para o arquivo de credenciais do Google Cloud (JSON)
 variable "credentials_file_path" {
   type = string
 }

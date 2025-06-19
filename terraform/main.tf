f6928b671486ebaa0bb019c739e6b6f20b35b7d9
emilioanastacio <emilioanastacio@gmail.com>
2025-06-19 20:31:24 -0300

    teste3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-  credentials = file(var.credentials_file_path)
-}
-
 provider "helm" {
   kubernetes {
     host                   = google_container_cluster.primary.endpoint

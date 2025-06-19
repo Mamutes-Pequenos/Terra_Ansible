bf05f80014dba009b29e94c11b589ebe5b47a9f6
emilioanastacio <emilioanastacio@gmail.com>
2025-06-19 20:26:28 -0300

    teste2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  credentials = file(var.credentials_file_path)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
 resource "google_container_cluster" "gke_cluster" {
   name     = var.cluster_name
   location = var.zone
@@ -33,6 +48,7 @@ resource "helm_release" "prometheus_stack" {
     name       = "stack-prometheus"
     repository = "https://prometheus-community.github.io/helm-charts"
     chart      = "kube-prometheus-stack"
+    version          = "57.0.2"
 
     namespace        = "monitoramento"
     create_namespace = true
@@ -50,4 +66,5 @@ resource "helm_release" "prometheus_stack" {
             }
         })
     ]
+    depends_on = [google_container_cluster.primary]
 }

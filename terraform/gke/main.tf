1d6c92c8d01a046c257b9116c03a0309a4ebd0e2
emilioanastacio <emilioanastacio@gmail.com>
2025-06-20 22:22:57 -0300

    muitas alterações22

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -4,7 +4,8 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 2
   network    = var.network
-
+  
+  enable_autopilot = false
   ip_allocation_policy {}
 
     node_config {

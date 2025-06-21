26bc33d1ce06106f8ecf987caa0e9de5f90ba596
emilioanastacio <emilioanastacio@gmail.com>
2025-06-20 21:33:51 -0300

    muitas alterações17

--- terraform/gke/outputs.tf
+++ terraform/gke/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_name" {
   description = "Nome do cluster GKE criado"
-  value       = google_container_cluster.gke_cluster.name
+  value       = google_container_cluster.primary.name
 }
 
 output "cluster_endpoint" {
   description = "Endpoint do cluster GKE"
-  value       = google_container_cluster.gke_cluster.endpoint
+  value       = google_container_cluster.primary.endpoint
 }

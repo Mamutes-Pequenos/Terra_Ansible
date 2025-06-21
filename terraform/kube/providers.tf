95701acab28424104e661e97c6f06780f3a2e6f5
emilioanastacio <emilioanastacio@gmail.com>
2025-06-20 21:23:01 -0300

    muitas alterações14

--- terraform/kube/providers.tf
+++ terraform/kube/providers.tf
@@ -1,11 +1,5 @@
 data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host = "http://{var.cluster_endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-}
-
 provider "helm" {
   kubernetes {
     host  = "http://${var.cluster_endpoint}"
@@ -13,4 +7,3 @@ provider "helm" {
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
 }
-

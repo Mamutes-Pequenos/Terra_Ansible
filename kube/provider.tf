905fc5f466bcd7d382088ed1b7e2d6f600927a12
emilioanastacio <emilioanastacio@gmail.com>
2025-06-21 19:33:50 -0300

    agora com o banco

--- kube/provider.tf
+++ kube/provider.tf
@@ -8,7 +8,7 @@ provider "kubernetes" {
 
 # Configura o provedor do Helm, que usa a conexão do provedor Kubernetes
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host  = "https://${var.gke_cluster_endpoint}"
     token = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(var.gke_cluster_ca_certificate)

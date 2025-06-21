0fef4c8cd62687e8eb75fec99174945bdd766d3c
emilioanastacio <emilioanastacio@gmail.com>
2025-06-21 16:49:31 -0300

    muitas alterações24

--- terraform/kube/providers.tf
+++ terraform/kube/providers.tf
@@ -1,14 +1,14 @@
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
+  host                   = google_container_cluster.primary.endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = "https://${google_container_cluster.primary.endpoint}"
+  "kubernetes" {
+    host                   = google_container_cluster.primary.endpoint
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
